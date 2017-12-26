b135797c8e14d577d6d43014f901642f2ecc5dda
Vistor25 <victorvoronko@gmail.com>
2017-12-26 13:28:06 +0300

    Add project files.

---
+++ CustomNuGetPackage/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomNuGetPackage
+{
+    public class Class1
+    {
+        HttpClient httpClient = new HttpClient();
+    }
+}
---
+++ StyleCopCustomRules/ControllerSuffixRule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StyleCop.CSharp;
+using StyleCop;
+
+namespace StyleCopCustomRules
+{
+    [SourceAnalyzer(typeof(CsParser))]
+    public class ControllerSuffixRule : SourceAnalyzer
+    {
+        private const string ruleName = "ControllerSuffixRule";
+
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            CsDocument doc = (CsDocument)document;
+            doc.WalkDocument(new CodeWalkerElementVisitor<object>(this.VisitElement));
+        }
+
+        private bool VisitElement(CsElement element, CsElement parentElement, object context)
+        {
+            var classElement = element as Class;
+
+            if (classElement != null)
+            {
+                if (classElement.BaseClass == "Controller" || classElement.BaseClass == "System.Web.Controller")
+                {
+                    if (!classElement.Name.EndsWith("Controller", System.StringComparison.Ordinal))
+                    {
+                        this.AddViolation(element, ruleName);
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
